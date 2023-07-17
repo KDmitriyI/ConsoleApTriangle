b1f9c2097344605b650f11dc49da4e1956ad9af3
Dmitriy Kislitsyn <kislitsynd@yandex.ru>
2023-07-17 22:06:30 +0300

    Добавил копию строчки в Module1

---
+++ ConsoleApTriangle/ConsoleApplication2/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim a, b, c As Single
+        a = Val(Console.ReadLine())
+        b = Val(Console.ReadLine())
+        c = Val(Console.ReadLine())
+        If a <= 0 Or b <= 0 Or c <= 0 Then Console.WriteLine("Условие неверно")
+        If a >= b + c Or b >= a + c Or c >= b + a Then Console.WriteLine("Треугольник со сторонами {0} {1} {2} несуществует", a, b, c) Else Console.WriteLine("Треугольник со сторонами {0} {1} {2} существует", a, b, c)
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ ConsoleApTriangle/ConsoleApplication2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором 
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("ConsoleApplication2")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ConsoleApplication2")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("ee53d462-4326-44e1-8eb6-71295d56a988")> 
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии 
+'      Номер построения
+'      Редакция
+'
+' Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
